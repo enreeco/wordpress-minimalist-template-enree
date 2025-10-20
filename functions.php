37141779255562462fe589dbf93b7b9eb7127922
Enrico Murru <enrico.murru@gmail.com>
2025-10-20 23:09:13 +0200

    Update version to 1.0.2 in style.css and functions.php, enhance menu toggle styles for improved responsiveness, and add menu icon image.

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ add_action( 'widgets_init', 'enree_widgets_init' );
  * Enqueue stili e script
  */
 function enree_scripts() {
-  wp_enqueue_style( 'enree-style', get_stylesheet_uri(), array(), '1.0.0' );
+  wp_enqueue_style( 'enree-style', get_stylesheet_uri(), array(), '1.0.2' );
   wp_enqueue_script( 'enree-nav', get_template_directory_uri() . '/js/navigation.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'enree_scripts' );
